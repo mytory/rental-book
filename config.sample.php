64e393d11038dcef555d7ac9476251a853a9cbb4
mytory <mytory@gmail.com>
2014-07-11 22:18:13 +0900

    try catch block.

--- config.sample.php
+++ config.sample.php
@@ -1,3 +1,13 @@
 <?php
 header('content-type: text/html; charset=utf-8');
-$db = new PDO('mysql:host=localhost;dbname=dbname;charset=utf8', 'username', 'password');
+
+$dbname = '';
+$username = '';
+$password = '';
+
+try {
+    $db = new PDO('mysql:host=localhost;dbname=' . $dbname . ';charset=utf8', $username, $password);
+} catch(PDOException $e) {
+    echo $e->getMessage();
+    exit;
+}
