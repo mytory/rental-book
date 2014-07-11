a2d413ffd758a0c1abd2d05d60191892fb9e8875
mytory <mytory@gmail.com>
2014-07-11 23:04:54 +0900

    recover.

--- save.php
+++ save.php
@@ -4,9 +4,15 @@ var_dump($_POST);
 
 $sql = "INSERT INTO list
         (빌려간사람, 빌린시각, 언제까지, 노트북번호)
-        VALUES ('{$_POST['빌려간사람']}','{$_POST['빌린시각']}','{$_POST['언제까지']}','{$_POST['노트북번호']}')";
+        VALUES (?,?,?,?)";
+$prepared = $db->prepare($sql);
 
-if( ! $db->query($sql)){
+if( ! $prepared->execute(array(
+    $_POST['빌려간사람'],
+    $_POST['빌린시각'],
+    $_POST['언제까지'],
+    $_POST['노트북번호'],
+))){
     var_dump($db->errorInfo());
     exit;
 }
